ded838c4e377e75fa213a09acc8625be4bf9d384
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2023-02-17 23:12:49 +0500

    simple animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <div className="flex justify-center items-center h-screen m-auto bg-gray-800">
-        <h1 className="text-3xl font-bold text-white ring-1">
+      <motion.div
+        className="border border-white rounded-lg p-8"
+        initial={{ opacity: 0 }}
+        animate={{ x: 100 }}>
+        <h1 className="text-3xl font-bold text-white">
           Hello world!
         </h1>
+      </motion.div>
     </div>
   );
 }
