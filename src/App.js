44efd69a1e2d24970674da9de62c0f0888e11473
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2023-02-17 23:13:38 +0500

    simple animations changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ function App() {
     <div className="flex justify-center items-center h-screen m-auto bg-gray-800">
       <motion.div
         className="border border-white rounded-lg p-8"
-        initial={{ opacity: 0 }}
-        animate={{ x: 100 }}>
+        animate={{
+          x: 0,
+          backgroundColor: "#000",
+          boxShadow: "10px 10px 0 rgba(0, 0, 0, 0.2)",
+          position: "fixed",
+          transitionEnd: {
+            display: "none",
+          },
+        }}>
         <h1 className="text-3xl font-bold text-white">
           Hello world!
         </h1>
