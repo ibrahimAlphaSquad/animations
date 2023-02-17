8ee1cbabf64901ad9bbff5ac0ad1e0ebabe0b30f
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2023-02-17 22:48:13 +0500

    framer-motion added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen m-auto bg-gray-800">
+        <h1 className="text-3xl font-bold text-white ring-1">
+          Hello world!
+        </h1>
     </div>
   );
 }
