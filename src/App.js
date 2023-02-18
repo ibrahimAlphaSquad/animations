8faa6b003093571737056e0f58bc37abb3360941
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2023-02-18 10:03:25 +0500

    position x

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
     <div className="flex justify-center items-center h-screen m-auto bg-gray-800">
       <motion.div
         className="border border-white rounded-lg p-8"
-        initial={{ scale: 0 }}
+        initial={{ scale: 0,x:100 }}
         animate={{
           scale: 1,
           rotate: 360,
-          // x: 0,
-          // backgroundColor: "#000",
+          x: 0,
+          backgroundColor: "#000",
           boxShadow: "10px 10px 0 rgba(0, 0, 0, 0.2)",
         }}
         transition={{
