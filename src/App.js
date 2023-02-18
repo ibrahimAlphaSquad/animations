b48f0de5e494a060cadb25b612c5ef4f19958538
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2023-02-18 11:21:12 +0500

    dynamic x and y axis values set via range

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 
 function App() {
+  const [rangeX_axis, setRangeX_axis] = useState(0);
+  const [rangeY_axis, setRangeY_axis] = useState(0);
   return (
     <div className="flex justify-center items-center h-screen m-auto bg-gray-800">
       <motion.div
         className="border border-white rounded-lg p-8"
-        initial={{ scale: 0,x:100 }}
+        initial={{ scale: 0, x: 100 }}
         animate={{
           scale: 1,
           rotate: 360,
@@ -17,7 +20,41 @@ function App() {
           duration: 3,
         }}
       >
-        <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+        <motion.h1
+          animate={{
+            x: rangeX_axis + "px",
+            y: rangeY_axis + "px",
+          }}
+          className="text-3xl font-bold text-white"
+        >
+          Hello world!
+        </motion.h1>
+        <div className="flex justify-center items-center space-x-2">
+          <label className="text-xl font-bold text-white" htmlFor="rangeX_axis">X :</label>
+          <input
+            id="rangeX_axis"
+            type="range"
+            max={100}
+            min={-100}
+            value={rangeX_axis}
+            onChange={(e) => {
+              setRangeX_axis(e.target.value);
+            }}
+          />
+        </div>
+        <div className="flex justify-center items-center space-x-2">
+          <label className="text-xl font-bold text-white" htmlFor="rangeY_axis">Y :</label>
+          <input
+            id="rangeY_axis"
+            type="range"
+            max={100}
+            min={-100}
+            value={rangeY_axis}
+            onChange={(e) => {
+              setRangeY_axis(e.target.value);
+            }}
+          />
+        </div>
       </motion.div>
     </div>
   );
